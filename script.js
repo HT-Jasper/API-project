ffc8bc3a7f4ae087c3cf70d63c66795c3a21b514
HT-Jasper <jasper.huytran@gmail.com>
2026-03-03 14:43:49 -0800

    complete API

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-fetch("https://api.open-meteo.com/v1/forecast?latitude=45.52&longitude=-122.68&hourly=temperature_2m,&hourly=apparent_temperature,&daily=weather_code")
-  .then(response => response.json())
-  .then(data => {
-    console.log("Weather data:", data);
-  })
-  .catch(error => {
-    console.error("Error fetching weather:", error);
-  });
-
 const cityName = document.getElementById("cityInput").value;
 const searchBtn = document.getElementById("citySearchBtn");
 
